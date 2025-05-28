68c20d935549be0b9043bc67a97803ab6087a632
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-28 16:01:15 +0300

    update

--- nvim/lua/configs/remaps.lua
+++ nvim/lua/configs/remaps.lua
@@ -27,30 +27,6 @@ set("x", "<leader>p", '"_dP')
 set({ "n", "v" }, "<leader>y", '"+y', { desc = "copy to clipboard" })
 
 set("n", "<leader>d", '"_d')
-set("v", "<leader>v", '"_d')
-
-set("n", "<leader>pv", ":Ex", { desc = "Go to File explorer" })
-
-set("i", "<C-c>", "<Esc>")
-set("n", "Q", "<nop")
-set("n", "<C-f>", "<cmd>silent !tmux neww tmuxsessionizer<CR>")
-
--- set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- set("n", "<leader>x", "<cmd>!chmod +x %<CR>", opts)
-
--- Increment/decrement
-set("n", "+", "<C-a>")
-set("n", "-", "<C-x>")
-
--- When we delete text in normal mode or visual mode using c, d or x that text goes to a register.
--- This affects the text we paste with the keybinding p.
--- What this do is modify x and X to delete text without changing the internal registers.
-set({ "n", "x" }, "x", '"_x')
-set({ "n", "x" }, "X", '"_d')
-
--- quickfix navigation
-set("n", "<leader>j", "<cmd>lnext<CR>zz")
-set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 -- plugins
 -- files
