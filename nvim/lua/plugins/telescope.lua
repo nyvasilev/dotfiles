96ea09709542caa893e620fb4d786432706ae3eb
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-06-11 23:59:45 +0300

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,62 +1,59 @@
-return {}
--- return {
--- 	{
--- 		"nvim-telescope/telescope.nvim",
--- 		tag = "0.1.8",
--- 		dependencies = {
--- 			"nvim-lua/plenary.nvim",
--- 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
--- 		},
--- 		config = function()
--- 			local harpoon_add_mark = function(prompt_bufnr)
--- 				local entry = require("telescope.actions.state").get_selected_entry()
--- 				local list = require("harpoon"):list()
--- 				local harpoon_config = list.config
--- 				local item = harpoon_config.create_list_item(harpoon_config, entry[1])
--- 				list:add(item)
--- 			end
--- 			require("telescope").setup({
--- 				defaults = {
--- 					layout_strategy = "bottom_pane",
--- 					-- layout_strategy = "vertical",
--- 					sorting_strategy = "ascending",
--- 					layout_config = {
--- 						height = 0.8,
--- 						width = 0.8,
--- 						prompt_position = "top",
--- 						preview_width = 0.4,
--- 					},
--- 					mappings = {
--- 						i = {
--- 							["<C-r>"] = harpoon_add_mark,
--- 						},
--- 						n = {
--- 							["<C-r>"] = harpoon_add_mark,
--- 						},
--- 					},
--- 				},
--- 				pickers = {
--- 					-- find_files = { theme = "ivy" },
--- 					-- git_files = { theme = "ivy" },
--- 					-- live_grep = { theme = "ivy" },
--- 					-- help_tags = { theme = "ivy" },
--- 					-- buffers = { theme = "ivy" },
--- 					-- lsp_definitions = { theme = "ivy" },
--- 					-- lsp_references = { theme = "ivy" },
--- 					-- git_commits = { theme = "ivy" },
--- 					-- git_bcommits = { theme = "ivy" },
--- 				},
--- 				extensions = {
--- 					fzf = {
--- 						fuzzy = true, -- false will only do exact matching
--- 						override_generic_sorter = true, -- override the generic sorter
--- 						override_file_sorter = true, -- override the file sorter
--- 					},
--- 				},
--- 			})
--- 			-- To get fzf loaded and working with telescope, you need to call
--- 			-- load_extension, somewhere after setup function:
--- 			require("telescope").load_extension("fzf")
--- 		end,
--- 	},
--- }
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local harpoon_add_mark = function(prompt_bufnr)
+			local entry = require("telescope.actions.state").get_selected_entry()
+			local list = require("harpoon"):list()
+			local harpoon_config = list.config
+			local item = harpoon_config.create_list_item(harpoon_config, entry[1])
+			list:add(item)
+		end
+		require("telescope").setup({
+			defaults = {
+				layout_strategy = "bottom_pane",
+				-- layout_strategy = "vertical",
+				sorting_strategy = "ascending",
+				layout_config = {
+					height = 0.8,
+					width = 0.8,
+					prompt_position = "top",
+					preview_width = 0.4,
+				},
+				mappings = {
+					i = {
+						["<C-r>"] = harpoon_add_mark,
+					},
+					n = {
+						["<C-r>"] = harpoon_add_mark,
+					},
+				},
+			},
+			pickers = {
+				-- find_files = { theme = "ivy" },
+				-- git_files = { theme = "ivy" },
+				-- live_grep = { theme = "ivy" },
+				-- help_tags = { theme = "ivy" },
+				-- buffers = { theme = "ivy" },
+				-- lsp_definitions = { theme = "ivy" },
+				-- lsp_references = { theme = "ivy" },
+				-- git_commits = { theme = "ivy" },
+				-- git_bcommits = { theme = "ivy" },
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+				},
+			},
+		})
+		-- To get fzf loaded and working with telescope, you need to call
+		-- load_extension, somewhere after setup function:
+		require("telescope").load_extension("fzf")
+	end,
+}
--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -16,7 +16,7 @@ return {
 		},
 		{
 			-- Open in the current working directory
-			"<leader>cw",
+			"<leader>",
 			"<cmd>Yazi cwd<cr>",
 			desc = "Open the file manager in nvim's working directory",
 		},
@@ -29,7 +29,7 @@ return {
 	---@type YaziConfig | {}
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
+		open_for_directories = true,
 		keymaps = {
 			show_help = "<f1>",
 		},
@@ -37,7 +37,7 @@ return {
 	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
 		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
